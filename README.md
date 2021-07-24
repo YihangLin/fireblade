3fbe3bcb22fa5c4bd71034b511fc0cc12faeddff
YihangLin <yihanglin0529@gmail.com>
2021-07-24 00:54:04 -0300

    frist commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#A Simple Landing page for a motorcylce
+
+Built with HTML, CSS and Swiper JS.
+
+[View it Online!](https://weather-d3d96.web.app/cbr.html)
---
+++ main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+  // autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 100,
+});
